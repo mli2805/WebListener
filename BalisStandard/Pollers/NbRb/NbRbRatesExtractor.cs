9a7b5e6ff51a0772ab70bbb775fbf940f5127fd9
mli2805 <mli2805@gmail.com>
2020-11-16 10:28:22 +0300

    url for nbrb changed

--- BalisStandard/Pollers/NbRb/NbRbRatesExtractor.cs
+++ BalisStandard/Pollers/NbRb/NbRbRatesExtractor.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
@@ -9,12 +8,11 @@ using Newtonsoft.Json;
 
 namespace BalisStandard
 {
-
     public static class NbRbRatesExtractor
     {
         public static async Task<NbRates> GetNbDayAsync(DateTime date)
         {
-            string uri = "http://www.nbrb.by/API/ExRates/Rates?onDate=" + $"{date:yyyy-M-d}" + "&Periodicity=0";
+            string uri = "https://www.nbrb.by/API/ExRates/Rates?onDate=" + $"{date:yyyy-M-d}" + "&Periodicity=0";
             var json = await ((HttpWebRequest)WebRequest.Create(uri))
                 .GetDataAsync();
             if (string.IsNullOrEmpty(json))
@@ -30,35 +28,6 @@ namespace BalisStandard
             var rubRate = nbList.First(c => c.Cur_Abbreviation == "RUB");
             result.Rub = rubRate.Cur_OfficialRate;
             return result;
-        }
-
-        public static async Task<NbRates> GetRatesForDate(DateTime date)
-        {
-            string uri = "http://www.nbrb.by/API/ExRates/Rates?onDate=" + $"{date:yyyy-M-d}" + "&Periodicity=0";
-            var response = await GetAsync(uri);
-            var nbList = (List<NbRbSiteRate>)JsonConvert.DeserializeObject(response, typeof(List<NbRbSiteRate>));
-            if (nbList.Count == 0) return null; 
-            var result = new NbRates(){Date = date};
-            var usdRate = nbList.First(c => c.Cur_Abbreviation == "USD");
-            result.Usd = usdRate.Cur_OfficialRate;
-            var euroRate = nbList.First(c => c.Cur_Abbreviation == "EUR");
-            result.Eur = euroRate.Cur_OfficialRate;
-            var rubRate = nbList.First(c => c.Cur_Abbreviation == "RUB");
-            result.Rub = rubRate.Cur_OfficialRate;
-            return result;
-        }
-
-        private static async Task<string> GetAsync(string uri)
-        {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-            HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
-            Stream stream = response.GetResponseStream();
-            if (stream == null) return "";
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                return await reader.ReadToEndAsync();
-            }
         }
 
         [SuppressMessage("ReSharper", "InconsistentNaming")]
